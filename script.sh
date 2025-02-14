b193ba4253883127a2e0e90bd36d0a9b63813936
Mutapha Pelumi Babatunde <phamust1111@gmail.com>
2025-02-14 17:51:16 +0000

    added script

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Move Nginx configuration file
+sudo cp /home/oxblixxx/fast-deploy/fastapi-book-project/fast-api /etc/nginx/sites-available/fast-api
+
+  # Create a symbolic link
+sudo ln -sf /etc/nginx/sites-available/fast-api /etc/nginx/sites-enabled/
