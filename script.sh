95dd3778655c6b1151ca4550a5fb0cb4e92e3c87
Mutapha Pelumi Babatunde <phamust1111@gmail.com>
2025-02-14 18:26:35 +0000

    modified script

--- script.sh
+++ script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Move Nginx configuration file
-sudo cp /home/oxblixxx/fast-deploy/fastapi-book-project/fast-api /etc/nginx/sites-available/fast-api
-
+sudo /bin/bash -c cp /home/oxblixxx/fast-deploy/fastapi-book-project/fast-api /etc/nginx/sites-available/fast-api
   # Create a symbolic link
-sudo ln -sf /etc/nginx/sites-available/fast-api /etc/nginx/sites-enabled/
-
+sudo /bin/bash -c ln -sf /etc/nginx/sites-available/fast-api /etc/nginx/sites-enabled/
 ##change permision
